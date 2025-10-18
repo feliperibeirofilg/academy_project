7d2b9203af1a56e4b38977d9d4e809d2d2d0e923
Felipe Ribeiro <felipearaujorib@icloud.com>
2025-10-18 19:20:47 -0300

    Add

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use App\Http\Middleware\UserMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,7 +12,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(userMiddleware)
+        // Use ::class para referenciar a classe do middleware
+        $middleware->append(UserMiddleware::class); // <-- CORREÇÃO AQUI
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
